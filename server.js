6009125a52870c0833ff29a4e3e2bc82b9a7c61b
torresquintero <alexandra.torresquintero@gmail.com>
2015-10-09 03:57:53 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/lively-wolf', function(request, response){
+    response.send("lively-wolf");
+});
+
+
 app.listen(process.env.PORT || 4000);
